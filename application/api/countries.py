c81ac792e2d928dd48ef7574a83c39d5a30d341f
migetonni <mixu03@gmail.com>
2022-12-08 17:38:26 +0200

    countries.py flask lisäosaan tehty funktio, joka hakee maat tietokannasta database_connectorin kautta

--- application/api/countries.py
+++ application/api/countries.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, Response
 import json
+from application.database_connector import get_countries
 
 countries = Blueprint('countries', __name__, url_prefix='/countries')
 
 @countries.route('/')
 def get_countries():
-    pass
+    countries = get_countries()
+    return countries
