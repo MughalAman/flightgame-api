6db357dd5468794a5103e9533efb343c38eff866
Karhu <santtu.karhu@live.fi>
2022-12-09 16:19:41 +0200

    Added more filters and results for database_connector.py Added json creation and airport picker for countries.py

--- application/api/countries.py
+++ application/api/countries.py
@@ -1,10 +1,34 @@
 from flask import Blueprint, Response
 import json
+from random import randint
 from application.database_connector import get_countries
 
 countries = Blueprint('countries', __name__, url_prefix='/countries')
 
 @countries.route('/')
 def get_countries():
-    countries = get_countries()
-    return countries
+    list_of_countries = get_countries()
+    sql_return = get_random_airport(list_of_countries)
+    result = turn_into_json(sql_return)
+    return result
+
+def get_random_airport(list_of_airports):
+    # 116 viimeinen lista lentokenttä
+    airport_num = randint(0, 116)
+    final_airport = list_of_airports[airport_num]
+    return final_airport
+
+def turn_into_json(sql_list):
+    sql_print = {
+        "airport_name": sql_list[0][0],
+        "airport_ident": sql_list[0][1],
+        "country_name": sql_list[0][2],
+        "country_code": sql_list[0][3],
+        "cordinates": {
+            "lat": sql_list[0][4],
+            "lon": sql_list[0][5]
+        },
+        "price": 100
+    }
+    result = json.dumps(sql_print)
+    return result
--- application/database_connector.py
+++ application/database_connector.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+
 connection = mysql.connector.connect(
          host='127.0.0.1',
          port= 3306,
@@ -11,9 +12,16 @@ connection = mysql.connector.connect(
 
 
 
-def get_countries():
+def get_countries(country_ident):
+    sql_name = f"SELECT airport.name, country.name, airport.ident, airport.iso_country, latitude_deg, longitude_deg " \
+             f"from airport LEFT JOIN country ON airport.iso_country = country.iso_country " \
+             f"where airport.continent = 'EU' and airport.type = 'large_airport' " \
+             f"order BY country.name, airport.name ASC ;"
+
     cursor = connection.cursor()
-    cursor.execute(f"Select * from countries")
+    cursor.execute(sql_name)
     result = cursor.fetchall()
+
     return result
+
 
