0c99062b084f241fe4d424a5ee86b817b59b3277
mikoh <miko.heino@metropolia.fi>
2022-12-13 13:38:27 +0200

    Added Jobs_dictionary.py

---
+++ application/api/Jobs_dictionary.py
@@ -0,0 +1,18 @@
+all_jobs = {
+        "Väärennä isoäitisi testamentti": "Testamentissa oli kirjoitusvirhe ja sinua syytetään väärennysrikoksesta.",
+        "Varasta rahaa mummoilta": "Mummo osasi Brasilialaista jiu-jitsua ja mursi niskasi",
+        "Myy aseita":"Laatikossa ollut käsikranaatti räjäytti myyntihetkellä sinut ja asiakkaasi",
+        "Myy itseäsi" : "Sinut kidnapattiin ja vietiin Pohjois-Koreaan työleirille.",
+        "Salamurhaa joku": "Olit huolimaton ja liukastuit verilammikkoon. Kaaduit suoraan veitsesi päälle.",
+        "Varasta ja myy wolt-tilauksia" : "Poliisit näkivät varkauden ja pidättivät sinut.",
+        "Myy elimiä" : "Lupasit ostajille liikaa ja he ottivat maksuksi molemmat silmäsi",
+        "Ryöstä pankki" : "Vartijat taklasivat sinut ennen kun ehdit vetää aseesi esiin.",
+        "Myy sarvikuonon sarvia": "Asiakkaasi oli salapoliisi joka ilmiantoi sinut",
+        "Varasta punaiselta ristiltä": "Punainen risti sai sinut kiinni ja vei kaiken veresi." ,
+
+        "Haravoi" : "", "Tee töitä mäkkärissä,", "Pese autoja", "Siivoa kauppakeskusta", "Aja nurmikkoa",
+        "Myy keksimääsi virvoitusjuomaa", "Kudo sukkia ja myy niitä", "Leikkaa hiuksia", "Kiillota kenkiä",
+        "Myy leivoksia", "Siivoa vessoja", "Myy kiviä", "Myy omenoita", "Myy päärynöitä",
+        "Anna hierontoja", "Kerää banaanaeja ja myy niitä", "Voita shakkiturnaus", "Myy tekemiäsi housuja",
+        "Myy fidget spinnereitä", "Leikkaa partoja"
+    }
--- application/api/jobs.py
+++ application/api/jobs.py
@@ -2,3 +2,4 @@ from flask import Blueprint, Response
 import json
 
 jobs = Blueprint('jobs', __name__, url_prefix='/jobs')
+
