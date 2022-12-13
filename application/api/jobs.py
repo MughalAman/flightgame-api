e2ee463631bd252817a95b28f2eb130046b5d018
migetonni <mixu03@gmail.com>
2022-12-13 14:18:17 +0200

    Displayaa työt

--- application/api/jobs.py
+++ application/api/jobs.py
@@ -1,5 +1,17 @@
 from flask import Blueprint, Response
 import json
+from Jobs_dictionary import all_jobs
+
+
 
 jobs = Blueprint('jobs', __name__, url_prefix='/jobs')
+
+@jobs.route('/')
+def job_displayer():
+    result = all_jobs
+    print(json.dumps(result))
+
+
+
+
 
