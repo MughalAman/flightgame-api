f9e066f8f73988d296f47f93ed60c1a9c4a1c664
Karhu <santtu.karhu@live.fi>
2022-12-15 19:12:39 +0200

    countries.py extended

--- application/api/countries.py
+++ application/api/countries.py
@@ -11,11 +11,21 @@ def get_countries_data():
     list_of_countries = get_countries()
     airports = []
     while len(airports) < 3:
-        sql_return = get_random_airport(list_of_countries)
-        airport = turn_into_json(sql_return)
+        sql_list = get_random_airport(list_of_countries)
+        airport = \
+        {"airport_name": sql_list[0],
+        "airport_ident": sql_list[2],
+        "country_name": sql_list[1],
+        "country_code": sql_list[3],
+        "coordinates": {
+            "lat": sql_list[4],
+            "lon": sql_list[5]
+        },
+        "price": 100
+    }
         if airport not in airports:
             airports.append(airport)
-    return json.dumps({"airports": airports})
+    return airports
 
 def get_random_airport(list_of_airports):
     # 116 viimeinen lista lentokenttä
