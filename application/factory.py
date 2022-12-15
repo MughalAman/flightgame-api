a7c910d819568647741ca5da9bf28a18fd65a258
Amppazz <amppa.mughal@gmail.com>
2022-12-15 20:36:53 +0200

    cors

--- application/factory.py
+++ application/factory.py
@@ -1,19 +1,20 @@
 from flask import Flask
-from flask_cors import CORS
 from application.api.countries import countries
 from application.api.jobs import jobs
 from application.api.leaderboard import leaderboard
 from application.api.weather import weather
 from application.api.maps import maps
 
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+
     app.register_blueprint(countries)
     app.register_blueprint(maps)
     app.register_blueprint(jobs)
     app.register_blueprint(leaderboard)
     app.register_blueprint(weather)
 
-    CORS(app, resources={r"/*": {"origins": "*"}})
     return app
