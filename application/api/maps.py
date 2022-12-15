d0eb56d3b28560c729b02e030d4ec753a82b657b
Karhu <santtu.karhu@live.fi>
2022-12-15 04:05:10 +0200

    maps.py marking working with location 1 and 1 additional location. Locations 3 and 4 not showing up and might have to drop from usage.

--- application/api/maps.py
+++ application/api/maps.py
@@ -1,18 +1,24 @@
-from flask import Blueprint, Response
-import os
-from mapbox import Geocoder
+from flask import Blueprint, Response, request
+
+
 
 maps = Blueprint('maps', __name__, url_prefix='/maps')
 
-os.environ['MAPBOX_ACCESS_TOKEN'] = "pk.eyJ1Ijoic2FudHR1amsiLCJhIjoiY2xiamc0MnE3MDU4MTN1bm80aW4wMm1sMCJ9.K4uuOEAe5iOW6wtezCzUyw"
-geocoder = Geocoder(access_token=os.environ['MAPBOX_ACCESS_TOKEN'])
+maps_token = "pk.eyJ1Ijoic2FudHR1amsiLCJhIjoiY2xiamc0MnE3MDU4MTN1bm80aW4wMm1sMCJ9.K4uuOEAe5iOW6wtezCzUyw"
 
 @maps.route('/')
 def get_map():
+    args = request.args
+    start_lat = float(args.get("start1"))
+    start_lng = float(args.get("start2"))
+    end_lat1 = float(args.get("end1_1"))
+    end_lng1 = float(args.get("end1_2"))
+    end_lat2 = float(args.get("end2_1"))
+    end_lng2 = float(args.get("end2_2"))
+    end_lat3 = float(args.get("end3_1"))
+    end_lng3 = float(args.get("end3_2"))
 
-    response = geocoder.forward("Helsinki")
+    api_map = f"<img src='https://api.mapbox.com/styles/v1/mapbox/dark-v11/static/pin-s+ff0000({start_lat},{start_lng}),pin-s+8f00ff({end_lat1},{end_lng1}),pin-s+8f00ff({end_lat2},{end_lng2}),pin-s+8f00ff({end_lat3},{end_lng3})/20,57,3.1,0,0/1200x800?access_token={maps_token}'>"
 
-    coordinates = response.geojson()['features'][0]['geometry']['coordinates']
 
-    return f"<img src='https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/{coordinates[0]},{coordinates[1]},12.0,0,0/1200x800?access_token={os.environ['MAPBOX_ACCESS_TOKEN']}'>"
-
+    return api_map
