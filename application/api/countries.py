4a014977334a4d250f5e86fae3c9df5ed7589402
Karhu <santtu.karhu@live.fi>
2022-12-16 00:40:35 +0200

    countries.py extended

--- application/api/countries.py
+++ application/api/countries.py
@@ -2,6 +2,7 @@ from flask import Blueprint, Response, request
 import json
 from random import randint
 from application.database_connector import get_countries
+from geopy.distance import geodesic as distance_km
 
 
 countries = Blueprint('countries', __name__, url_prefix='/countries')
@@ -33,3 +34,28 @@ def get_random_airport(list_of_airports):
     final_airport = list_of_airports[airport_num]
     return final_airport
 
+
+def count_distance(start, end):
+    return round(distance_km(start, end).km, 2)
+
+def count_co2_consumed(distance):
+    return round((distance*0.176), 0)
+
+
+#http://127.0.0.1:5000/countries/flight_info?start_lat=10.10&start_lon=10.10&end_lat=20.20&end_lon=20.20
+@countries.route('/flight_info')
+def flight_info():
+    start_airport_lat = float(request.args.get("start_lat"))
+    start_airport_lon = float(request.args.get("start_lon"))
+    end_airport_lat = float(request.args.get("end_lat"))
+    end_airport_lon = float(request.args.get("end_lon"))
+    start_airport = (start_airport_lat, start_airport_lon)
+    end_airport = (end_airport_lat, end_airport_lon)
+    distance = count_distance(start_airport, end_airport)
+    result = count_co2_consumed(distance)
+    counted_info = {
+                    "distance": distance,
+                    "co2_consumed": result
+                    }
+
+    return Response(json.dumps(counted_info), mimetype='text/html', status=200)
